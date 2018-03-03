89b4375662e230b54d7cf4c3fd0c6d7eff6dfb2f
jeffrey <digitalpump@gmail.com>
2018-03-04 01:30:12 +0800

    调整日志输出格式

--- app/Providers/LogServiceProvider.php
+++ app/Providers/LogServiceProvider.php
@@ -24,8 +24,9 @@ class LogServiceProvider extends ServiceProvider
     {
         $maxFiles = 0;
         $filepath = '/logs/'.date('Y-m').'/api.log';
+        $format = "[%datetime%] %level_name%: %message% %context% %extra%\n";
         $handlers[] = (new RotatingFileHandler(storage_path($filepath), $maxFiles))
-            ->setFormatter(new LineFormatter(null, null, true, true));
+            ->setFormatter(new LineFormatter($format, null, true, true));
         $this->app['log']->setHandlers($handlers);
     }
 
---
+++ config/app.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeffrey
+ * Date: 2018/3/4
+ * Time: 01:15
+ */
+return [
+    'log_channel' => env('APP_LOG_CHANNEL', 'j'),
+];
