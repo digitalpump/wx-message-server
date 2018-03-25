a1eb1d743abc51c67725398f658ea1ef40dc6dc2
jeffrey <digitalpump@gmail.com>
2018-03-25 12:06:05 +0800

    创建api controller 和 middleware

---
+++ app/Http/Api/Controllers/JwtAuthController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeffrey
+ * Date: 2018/3/25
+ * Time: 12:03
+ */
+
+namespace App\Http\Api\Controllers;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class JwtAuthController extends Controller
+{
+
+    public function apiLogin(Request $request) {
+
+    }
+    
+}
