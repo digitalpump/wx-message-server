544f945a0cf1772ba1c331b43a7649bf1979d38c
digitalpump <digitalpump@gmail.com>
2018-03-30 00:12:06 +0800

    no message

--- app/Http/Api/Handlers/TextMessageHandler.php
+++ app/Http/Api/Handlers/TextMessageHandler.php
@@ -20,9 +20,25 @@ class TextMessageHandler implements EventHandlerInterface
         $msgId = $payload['MsgId'];
         $openid = $payload['FromUserName'];
         $content = $payload['Content'];
+
+        //帮助，命令行列表
+        //TODO 简单独立的命令优先级最高，比如查我的openid
+        if (trim($content)=="我是谁") {
+            return $openid;
+        }
         if ($openid=='ofSvBt7vapubGyEEZV9ktIIv__Ik') {
+
+            //TODO boss command.
             return "老板你好。";
         }
+
+        //TODO 记录用户command  和 组合command的状态机
+        /*
+         * 比如用户开始注册公众号
+         *
+         * 1 步骤      状态机 process_status 0 等待
+         */
+
         if (trim($content)=="我要上天") {
             //TODO 查用户有没有注册
             //有注册
@@ -32,5 +48,7 @@ class TextMessageHandler implements EventHandlerInterface
        //Log::debug("@TextMessageHandler from user:".$payload['FromUserName']);
        //Log::debug("@TextMessageHandler Content:".$payload['Content']);
     }
+
+
 
 }
